ecbea7ff3bb32c60005fd344065c9a58f27ab2af
fcvane@163.com <abc123>
2019-05-30 16:02:18 +0800

    MySQL数据库

--- Etrans/settings.py
+++ Etrans/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Etrans',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'Etrans.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # 或者使用 mysql.connector.django
+        'NAME': 'etrans',
+        'USER': 'etrans',
+        'PASSWORD': 'etrans',
+        'HOST':'10.45.15.205',
+        'PORT':'3306',
     }
 }
 
