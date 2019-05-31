266e5fefbd970dc1e2c433e276517025afc8e447
li.ke <328594663@qq.com>
2019-05-31 16:16:06 +0800

    add

--- Etrans/etrans/views.py
+++ Etrans/etrans/views.py
@@ -7,14 +7,8 @@ from django.http import HttpResponse
 
 
 def hello(request):
-    #return HttpResponse("Hello world ! ")
-    # context = {}
-    # context['hello'] = 'Hello World!'
      return render(request, "eBaseManage.html")
 
 
 def index(request):
-    #return HttpResponse("Hello world ! ")
-    # context = {}
-    # context['hello'] = 'Hello World!'
-     return render(request, "eBaseManage.html")
+    return render(request, "eBaseManage.html")
